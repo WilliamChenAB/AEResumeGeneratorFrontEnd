6e933823cb1839ffa5bbfbb6e0cf62a96b6381b4
Jack <34490671+jackisrong@users.noreply.github.com>
2022-02-20 18:28:09 -0800

    Redo bootstrap with create-react-app (#28)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Split from 'react-split';
 import Button from '@mui/material/Button';
 import AELogo from './assets/images/ae_logo_blue.png';
@@ -6,22 +5,22 @@ import './App.css';
 import TopBar from './components/TopBar';
 
 function App() {
-	return (
+  return (
 		<div className="App">
 			<TopBar buttons={['Resumes', 'Sectors']} roles={['Employee', 'Project Admin', 'System Admin']} />
 			<h1>Hello, World!</h1>
-			<img src={AELogo} />
+			<img src={AELogo} alt='Applied Engineering' />
 			<Split className='Split' direction='horizontal' sizes={[70, 30]} minSize={[300, 100]} gutterAlign='center' cursor='col-resize' >
 				<div className='my-box'>LEFT SIDE</div>
 				<div className='my-box'>RIGHT SIDE</div>
 			</Split>
 			<Button variant='contained' color='success'>Button Text</Button>
 			<br />
-			<img src={AELogo} />
+			<img src={AELogo} alt='Applied Engineering' />
 			<br />
-			<img src={AELogo} />
+			<img src={AELogo} alt='Applied Engineering' />
 			<br />
-			<img src={AELogo} />
+			<img src={AELogo} alt='Applied Engineering' />
 		</div>
 	);
 }
