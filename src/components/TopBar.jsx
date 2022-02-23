38b10a8f8264dd52d07cba1b84fb940e484b9304
Luis Nogales <66089631+luis-e-nogales@users.noreply.github.com>
2022-02-22 18:12:18 -0800

    Update top bar (#32)
    * Update TopBar.jsx
    
    * fixed how the logo looks

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
 import { AppBar, Box, Button, MenuItem, Select, Toolbar } from '@mui/material';
-import AELogo from '../assets/images/ae_logo_blue.png';
+import AELogoCrop from '../assets/images/ae_logo_blue_cropped.png';
 import { Person } from '@mui/icons-material';
 
 function TopBar({ buttons, roles }) {
-	// this is just a placeholder file. delete this if we don't end up using it!
-	return (
-		<div>
-			<AppBar>
-				<Toolbar>
-					<Box component='img' sx={{ height: 1 / 30, width: 1 / 30 }} alt='Associated Engineering' src={AELogo} />
-					<Box sx={{ flexGrow: 1 }}>
-						{
-							buttons.map(button => {
-								return (
-									<Button sx={{ color: 'white' }}>{button}</Button>
-								);
-							})
-						}
-					</Box>
 
-					<Box>
-						<Person sx={{ verticalAlign: 'middle' }} />
-						<Select sx={{ color: 'white' }} defaultValue='Employee' variant='standard'>
-							{
-								roles.map(role => {
-									return (
-										<MenuItem key={role} value={role}>{role}</MenuItem>
-									);
-								})
-							}
-						</Select>
-						<Button sx={{ color: 'white' }}>Sign Out</Button>
-					</Box>
-				</Toolbar>
-			</AppBar>
-			<Toolbar />
-		</div>
-	);
+  //Not sure if we should just use Jack's dropdown or if its fine to handle it differently because its only in 1 place
+  //might be best to just use a set of values for both that have each their own functions instead of passing it in
+
+  const handleChange = (ev) => {
+    // This function should probably end up calling another function that's passed in as a prop handling each element.
+    console.log(`Dropdown option changed to ${ev.target.value}`);
+  }
+
+  const handleMenuButtonClicked = (btnName) => {
+    // This function should probably end up calling another function that's passed in as a prop handling each element.
+    console.log(`Top menu button clicked: ${btnName}`);
+  }
+
+  const handleSignOutClicked = () => {
+    //add sign out logic?
+   console.log(`Sign out clicked`);
+  }
+
+  // this is just a placeholder file. delete this if we don't end up using it!
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Box component='img' sx={{ width: 50}} alt='Associated Engineering' src={AELogoCrop} />
+          <Box sx={{ flexGrow: 1}}>
+            {
+              buttons.map(button => {
+                return (
+                  <Button onClick={e => handleMenuButtonClicked(button)} key={button} sx={{ color: 'white' }}>{button}</Button>
+                );
+              })
+            }
+          </Box>
+
+          <Box>
+            <Person sx={{ verticalAlign: 'middle' }} />
+            <Select onChange={handleChange} sx={{ color: 'white' }} defaultValue='Employee' variant='standard'>
+              {
+                roles.map(role => {
+                  return (
+                    <MenuItem key={role} value={role}>{role}</MenuItem>
+                  );
+                })
+              }
+            </Select>
+            <Button onClick={handleSignOutClicked} sx={{ color: 'white' }}>Sign Out</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </div>
+  );
 }
 
 export default TopBar;
