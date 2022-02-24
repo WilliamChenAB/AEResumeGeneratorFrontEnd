120cde3a6f45a1468f8af508e53f8183082296dd
Luis Nogales <66089631+luis-e-nogales@users.noreply.github.com>
2022-02-24 14:28:43 -0800

    Add SidebarTab Component (#35)
    * added basic form of tabs
    
    * added fixes

---
+++ src/components/SideBarTabs.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import {List, ListItem , ListItemText, Checkbox, ListItemButton} from '@mui/material';
+import { Error } from '@mui/icons-material';
+
+
+/**
+ * SideBar Tab component
+ * Takes up full width and height of parent container, scrolls when entries dont fit in full height.
+ * @param entries array of names managed by parent along with their error status
+ * @param color not selected color in hex
+ * @param selectedColor selected tab color in hex
+ * @param textColor color of text in hex
+ * @param showCheckBoxes deciding on whether to show optional checkboxes
+ * @param onEntryClick click handler for selection
+ * @param onCheckmarkClicked onSelected hanlder for checkboxes
+ * @returns SideBarTabs component
+ */
+function SideBarTabs({entries, showCheckBoxes, color, selectedColor, textColor, onEntryClick, onCheckmarkClicked}){
+  const [selectedEntry, setSelectedEntry] = useState('');
+
+  return(
+  <List sx={{maxHeight: '100%', overflow: 'auto'}}>
+    {
+      entries.map(obj => {
+        const checkbox = (showCheckBoxes)? <Checkbox key = {obj.name} onChange={onCheckmarkClicked}
+        sx={{
+          color: textColor,
+          '&.Mui-checked': {
+            color: textColor,
+          },
+        }}></Checkbox> : null;
+        const icon = (obj.error)? <Error/> : null;
+        const usedColor = obj.name === selectedEntry? selectedColor: color;
+        return(
+          <ListItem key = {obj.name} sx={{background: usedColor, marginBottom: 0.1}} disablePadding>
+            <ListItemButton onClick={(ev) => 
+              {
+                setSelectedEntry(obj.name);
+                onEntryClick(ev);
+              }}>
+              {checkbox}
+              <ListItemText sx={{ color: textColor }}>
+                {obj.name}
+              </ListItemText>
+              {icon}
+            </ListItemButton>
+        </ListItem>);
+      })
+    }
+  </List>
+  );
+}
+
+export default SideBarTabs;
--- src/containers/LuisPlayground.jsx
+++ src/containers/LuisPlayground.jsx
@@ -1,7 +1,33 @@
+import SideBarTabs from '../components/SideBarTabs';
+import React from 'react';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+
+const ent = [{name: "Experience", error: true}, {name: "Education", error: false}, {name: "Summary", error: false}, {name: "Justification", error: false}];
+
+function clickHandler(ev){
+  console.log(`clicked }`);
+}
+
+function selectHandler(ev){
+  console.log(`selected ${ev}`);
+}
+
 
 function LuisPlayground() {
+  const [entries, setEntries] = useState(ent);
     return (
         <div>
+          <Box sx={{width: 500, height: 200}}>
+            <SideBarTabs 
+                entries={entries} 
+                showCheckBoxes={true} 
+                color='#87C34B' 
+                selectedColor='#00569C' 
+                textColor='white'
+                onEntryClick={clickHandler}
+                onCheckmarkClicked={selectHandler}/>
+          </Box>
         </div>
     );
 }
